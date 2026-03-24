7530cb50ff515c94820a7437e5324899e2cf4d7e
David Barile <davidbarile@gmail.com>
2026-03-24 00:39:39 -0700

    fix: convert to move camera

--- Assets/Scripts/World/DayNightCycleController.cs
+++ Assets/Scripts/World/DayNightCycleController.cs
@@ -5,18 +5,22 @@ using System.Collections;
 
 public class DayNightCycleController : MonoBehaviour
 {
+     [SerializeField] private Transform cameraTransform;
     [SerializeField] private Animation dayNightCycleAnim;
     [SerializeField] private RectTransform timeOfDayRectTrans;
-    [SerializeField] private float rectTransOffset;
+    [SerializeField] private float cameraOffset = -1920;
+
+    private Vector3 initCameraPos;
 
     private DateTime lastTimeSliderMoved;
     
-    private void OnEnable()
+    private void Start()
     {
+        this.initCameraPos = this.cameraTransform.localPosition;
         TimeManager.OnHourChanged += HandleHourChanged;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         TimeManager.OnHourChanged -= HandleHourChanged;
     }
@@ -44,23 +48,23 @@ public class DayNightCycleController : MonoBehaviour
             this.dayNightCycleAnim.CrossFade(clipName, 1f); // Smoothly transition to the new time of day
         }
 
-        SetTimeOfDayRectPosition(normalizedTime);
+        SetCameraPosition(normalizedTime);
 
         //Debug.Log($"currentHour: {currentHour} ({TimeManager.FormatFloatAsTime(currentHour)})   normalizedTime: {normalizedTime}   frame = {frameNum} / {clipLength}   anim speed = {this.dayNightCycleAnim[clipName].speed}");
     }
 
     // This method can be called by a UI slider to allow the user to pan world rect trans
-    public void UserPanWorld(float normalizedValue)
+    public void UserPanCamera(float normalizedValue)
     {
         var rectWidth = this.timeOfDayRectTrans.rect.width;
-        var scrollValue = ((normalizedValue * rectWidth) + this.rectTransOffset) * TimeManager.IN.HoursToSecondsRatio;
+        var scrollValue = -1 * ((normalizedValue * rectWidth) + this.cameraOffset);
         UiManager.IN.Compass.SetDirection(normalizedValue);
-        this.timeOfDayRectTrans.localPosition = new Vector3(scrollValue, 0, 0);
+        this.cameraTransform.localPosition = new Vector3(scrollValue, this.initCameraPos.y, this.initCameraPos.z);
 
         this.lastTimeSliderMoved = DateTime.Now;
     }
 
-    private void SetTimeOfDayRectPosition(float normalizedTime)
+    private void SetCameraPosition(float normalizedTime)
     {
         if (DateTime.Now - this.lastTimeSliderMoved < TimeSpan.FromSeconds(2))
             return; // Don't update the position if the user has recently moved the slider
@@ -68,30 +72,30 @@ public class DayNightCycleController : MonoBehaviour
         UiManager.IN.Compass.SetDirection(normalizedTime);
 
         var rectWidth = this.timeOfDayRectTrans.rect.width;
-        var newPosition = new Vector3(normalizedTime * rectWidth + this.rectTransOffset, 0, 0);
+        var newPosition = new Vector3(-1 * ((normalizedTime * rectWidth) + this.cameraOffset), this.initCameraPos.y, this.initCameraPos.z);
 
         if (TimeManager.IN.UseRealTime || Time.frameCount < 100)
         {
-            this.timeOfDayRectTrans.localPosition = newPosition;
+            this.cameraTransform.localPosition = newPosition;
             return;
         }
         
-        var delta = Math.Abs(this.timeOfDayRectTrans.localPosition.x - newPosition.x);
-        if (delta > 1000f && this.timeOfDayRectTrans.localPosition.x > rectWidth * 0.5f) // If the delta is large, jump to the new position without tweening
+        var delta = Math.Abs(this.cameraTransform.localPosition.x - newPosition.x);
+        if (delta > 1000f && this.cameraTransform.localPosition.x > rectWidth * 0.5f) // If the delta is large, jump to the new position without tweening
         {
-            var fakePosition = new Vector3((normalizedTime + 1) * rectWidth + this.rectTransOffset, 0, 0);
-            var fakeDelta = Math.Abs(this.timeOfDayRectTrans.localPosition.x - fakePosition.x);
-            this.timeOfDayRectTrans.localPosition = new Vector3(newPosition.x - fakeDelta, this.timeOfDayRectTrans.localPosition.y, this.timeOfDayRectTrans.localPosition.z); // Move to the opposite side before tweening
+            var fakePosition = new Vector3((normalizedTime * rectWidth) + this.cameraOffset, this.initCameraPos.y, this.initCameraPos.z);
+            var fakeDelta = Math.Abs(this.cameraTransform.localPosition.x - fakePosition.x);
+            this.cameraTransform.localPosition = new Vector3(newPosition.x - fakeDelta, this.cameraTransform.localPosition.y, this.cameraTransform.localPosition.z); // Move to the opposite side before tweening
             StartCoroutine(DelayedSetTimeOfDayRectPosition(newPosition));
             return;
         }
 
-        this.timeOfDayRectTrans.DOLocalMoveX(newPosition.x, 1f).SetEase(Ease.Linear);
+        this.cameraTransform.DOLocalMoveX(newPosition.x, 1f).SetEase(Ease.Linear);
     }
     
     private IEnumerator DelayedSetTimeOfDayRectPosition(Vector3 newPosition)
     {
         yield return new WaitForSeconds(0.05f); // Wait a short time before tweening to the new position    
-        this.timeOfDayRectTrans.DOLocalMoveX(newPosition.x, 0.95f).SetEase(Ease.Linear);
+        this.cameraTransform.DOLocalMoveX(newPosition.x, 0.95f).SetEase(Ease.Linear);
     }
 }
