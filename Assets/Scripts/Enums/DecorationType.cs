de3ee3ecd5fa74b1057a36d751387986fcd5adde
David Barile <davidbarile@gmail.com>
2026-04-03 10:57:36 -0700

    fix: added Sponge to default inventory
    added enum

--- Assets/Scripts/Enums/DecorationType.cs
+++ Assets/Scripts/Enums/DecorationType.cs
@@ -7,12 +7,13 @@ public enum EDecorationType
 
     // Passive Harvesters
     Bucket = 1,        // Collects water during rain
-    Jar = 2,           // Collects dew in the morning
+    Jar = 2,           // Collects fireflies in the evening
     FlowerPot = 3,     // Grows seeds into plants
     LightningRod = 4,  // Collects lightning energy
-    MoonCrystal = 5,   // Charges with moonbeams at night
+    Crystal = 5,   // Charges with moonbeams at night
     SpiderWeb = 6,     // Traps insects
     MagnifyingGlass = 7, // Focuses sunlight to generate heat
+    Sponge = 8,         // Absorbs dew
 
     // Visual Decorations
     Plant = 20,         // Corner leaf clusters
