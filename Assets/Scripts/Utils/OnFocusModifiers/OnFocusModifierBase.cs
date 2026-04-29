e946f2d4599b8fec1256a6df9f4ef022c0dbaaaa
David Barile <davidbarile@gmail.com>
2026-04-29 09:39:01 -0700

    fix: added EListenerType to OnFocusModifierBase

--- Assets/Scripts/Utils/OnFocusModifiers/OnFocusModifierBase.cs
+++ Assets/Scripts/Utils/OnFocusModifiers/OnFocusModifierBase.cs
@@ -3,18 +3,41 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public class OnFocusModifierBase : MonoBehaviour
 {
+    private enum EListernerType
+    {
+        OnGameFocusChanged,
+        OnMinimizeMaximizeToggled,
+        Both
+    }
+
+    [SerializeField] private EListernerType eventListenerType;
+
     [SerializeField] private bool shouldAppendObjectName;
     private string originalName;
 
     protected virtual void Start()
     {
         this.originalName = this.gameObject.name;
-        ScreenManager.OnGameFocusChanged += OnGameFocusChanged;
+
+        switch (this.eventListenerType)
+        {
+            case EListernerType.OnGameFocusChanged:
+                ScreenManager.OnGameFocusChanged += OnGameFocusChanged;
+                break;
+            case EListernerType.OnMinimizeMaximizeToggled:
+                ScreenManager.OnMinimizeMaximizeToggled += OnGameFocusChanged;
+                break;
+            case EListernerType.Both:
+                ScreenManager.OnGameFocusChanged += OnGameFocusChanged;
+                ScreenManager.OnMinimizeMaximizeToggled += OnGameFocusChanged;
+                break;
+        }
     }
 
     protected virtual void OnDestroy()
     {
         ScreenManager.OnGameFocusChanged -= OnGameFocusChanged;
+        ScreenManager.OnMinimizeMaximizeToggled -= OnGameFocusChanged;
     }
 
     protected virtual void OnGameFocusChanged(bool hasFocus)
