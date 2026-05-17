fe20bffb164aafbb89ba017bab38a8fcd3bb3975
David Barile <davidbarile@gmail.com>
2026-05-17 12:32:50 -0700

    fix: added sister knobs

--- Assets/Scripts/UI/UiDecorationEditKnob.cs
+++ Assets/Scripts/UI/UiDecorationEditKnob.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UiDecorationEditKnob : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] private DecorationBase decoration;
+    private enum ECorner
+    {
+        TopRight,
+        TopLeft,
+        BottomRight,
+        BottomLeft
+    }
+
+    [SerializeField] private ECorner cornerType;
+    [Space, SerializeField] private DecorationBase decoration;
     [SerializeField] private RectTransform editRT;
-    [SerializeField] private Transform corner;
     [SerializeField] private GameObject rect;
 
     [Header("Scale Settings")]
     [Range(0, 1), SerializeField] private float minScale = 0.5f;
     [Range(0, 5), SerializeField] private float maxScale = 3.0f;
+
+    [Header("Sister Knobs (Optional)")]
+    [SerializeField] private UiDecorationEditKnob[] sisterKnobs;
 
     private Vector3 initialSize, initialScale, initialRotation;
     private float initialDistance;
@@ -27,11 +39,11 @@ public class UiDecorationEditKnob : MonoBehaviour, IBeginDragHandler, IDragHandl
         this.canvas = this.editRT.GetComponentInParent<Canvas>();
 
         // Calculate initial distance and angle from Icon center to Corner (represents scale 1.0 and rotation 0)
-        this.initialDistance = CalculateDistanceFromIconCenter(this.corner.position);
-        this.initialAngle = CalculateAngleFromIconCenter(this.corner.position);
+        this.initialDistance = CalculateDistanceFromIconCenter(GetCornerPosition());
+        this.initialAngle = CalculateAngleFromIconCenter(GetCornerPosition());
 
         DragManager.OnDragModeChanged += OnDragModeChanged;
-        this.transform.position = this.corner.position;
+        this.transform.position = GetCornerPosition();
 
         if (this.rect)
             this.rect.SetActive(DragManager.IsDragModeActivated);
@@ -52,8 +64,8 @@ public class UiDecorationEditKnob : MonoBehaviour, IBeginDragHandler, IDragHandl
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Recalculate initial distance and angle in case Icon was already transformed
-        this.initialDistance = CalculateDistanceFromIconCenter(this.corner.position);
-        this.initialAngle = CalculateAngleFromIconCenter(this.corner.position);
+        this.initialDistance = CalculateDistanceFromIconCenter(GetCornerPosition());
+        this.initialAngle = CalculateAngleFromIconCenter(GetCornerPosition());
 
          if (this.rect)
             this.rect.SetActive(true);
@@ -87,10 +99,16 @@ public class UiDecorationEditKnob : MonoBehaviour, IBeginDragHandler, IDragHandl
         
         // Force transform hierarchy update so Corner position is current
         Canvas.ForceUpdateCanvases();
-        
+
         // Now position the knob at the Corner's world position
         // The Corner has moved to where it should be based on the scale/rotation
-        this.transform.position = this.corner.position;
+        this.transform.position = GetCornerPosition();
+        
+        foreach (var sister in this.sisterKnobs)
+        {
+            if (sister != null)
+                sister.transform.position = sister.GetCornerPosition();
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -110,13 +128,34 @@ public class UiDecorationEditKnob : MonoBehaviour, IBeginDragHandler, IDragHandl
         
         return Vector2.Distance(iconCenter, targetScreen);
     }
-    
+
     private float CalculateAngleFromIconCenter(Vector3 worldPosition)
     {
         Vector2 iconCenter = RectTransformUtility.WorldToScreenPoint(this.canvas.worldCamera, this.editRT.position);
         Vector2 targetScreen = RectTransformUtility.WorldToScreenPoint(this.canvas.worldCamera, worldPosition);
-        
+
         Vector2 delta = targetScreen - iconCenter;
         return Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+    }
+    
+    public Vector3 GetCornerPosition()
+    {
+        Vector3 localPos = Vector3.zero;
+        switch (this.cornerType)
+        {
+            case ECorner.TopRight:
+                localPos = new Vector3(this.initialSize.x / 2, this.initialSize.y / 2, 0);
+                break;
+            case ECorner.TopLeft:
+                localPos = new Vector3(-this.initialSize.x / 2, this.initialSize.y / 2, 0);
+                break;
+            case ECorner.BottomRight:
+                localPos = new Vector3(this.initialSize.x / 2, -this.initialSize.y / 2, 0);
+                break;
+            case ECorner.BottomLeft:
+                localPos = new Vector3(-this.initialSize.x / 2, -this.initialSize.y / 2, 0);
+                break;
+        }
+        return this.editRT.TransformPoint(localPos);
     }
 }
